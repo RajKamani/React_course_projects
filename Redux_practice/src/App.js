fab4214b5693bc8ee22021d801b3202e92f0c92c
Raj Kamani <rajKamani2001@gmail.com>
2021-12-13 13:41:20 +0530

    cart done

--- Redux_practice/src/App.js
+++ Redux_practice/src/App.js
@@ -1,15 +1,46 @@
-import { useSelector } from "react-redux";
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { sendCartdata } from "./store/cart-slice";
+import { fetchCart } from "./store/cart-slice";
+
+let isinit = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const notification = useSelector((state) => state.ui.notification);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCart());
+  }, [dispatch]);
+  useEffect(() => {
+    if (isinit) {
+      isinit = false;
+      return;
+    }
+    if (cart.changed) {
+      dispatch(sendCartdata(cart));
+    }
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.msg}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />} <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- Redux_practice/src/store/cart-slice.js
+++ Redux_practice/src/store/cart-slice.js
@@ -1,16 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
+
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItem(state, action) {
       const newItem = action.payload;
 
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -28,6 +36,7 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.qty === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
@@ -37,6 +46,81 @@ const cartSlice = createSlice({
     },
   },
 });
+// complex :/
+export const fetchCart = () => {
+  return async (dispatch) => {
+    const fetchdata = async () => {
+      const res = await fetch(
+        "https://cart-9e3a0-default-rtdb.firebaseio.com/Cart.json"
+      );
+      const data = await res.json();
+      return data;
+    };
+    try {
+      const cartData = await fetchdata();
+      dispatch(
+        cartAction.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNoti({
+          status: "fetch error",
+          title: "Error..",
+          msg: "fetch task Failed..",
+        })
+      );
+    }
+  };
+};
+
+export const sendCartdata = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNoti({
+        status: "pending.",
+        title: "Sending..",
+        msg: "please wait..",
+      })
+    );
+
+    const sendReq = async () => {
+      const response = await fetch(
+        "https://cart-9e3a0-default-rtdb.firebaseio.com/Cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("failed");
+      }
+    };
+    try {
+      await sendReq();
+      dispatch(
+        uiActions.showNoti({
+          status: "success",
+          title: "success..",
+          msg: "saved.",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNoti({
+          status: "error",
+          title: "Error..",
+          msg: "task Failed..",
+        })
+      );
+    }
+  };
+};
 
 export const cartAction = cartSlice.actions;
 export default cartSlice;
